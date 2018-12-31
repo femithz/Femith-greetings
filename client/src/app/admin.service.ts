56bf817330818907201a47a87e99ddfb4ec17a6a
Ajayi Oluwafemi <ajayiadeyinka6991@gmail.com>
2018-12-31 02:56:03 +0100

    better

--- client/src/app/admin.service.ts
+++ client/src/app/admin.service.ts
@@ -6,7 +6,7 @@ export class AdminService {
 
   constructor(private _http: HttpClient) { }
   send(body: any) {
-    return this._http.post('http://127.0.0.1:3000/message', body, {
+    return this._http.post('/message', body, {
       observe: 'body',
       withCredentials: true,
       headers: new HttpHeaders().append('Content-Type', 'application/json')
